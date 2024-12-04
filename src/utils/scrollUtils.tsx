8bdeca4ceabef815d6fc8221730f1555c9c6bf67
Mathieu <bon.mathieu@gmail.com>
2024-12-04 11:53:17 +0100

    Removed comments.

--- src/utils/scrollUtils.tsx
+++ src/utils/scrollUtils.tsx
@@ -1,7 +1,5 @@
 export const scrollToSection = (sectionId: string) => {
-	console.log("FUNCTION scrollToSection")
 	const section = document.getElementById(sectionId)
-	console.log(`Section corresponding to '${sectionId}' : ${section}`)
 	if (section) {
 		section.scrollIntoView({ behavior: "smooth" })
 	}
