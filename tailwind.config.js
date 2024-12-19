0b91952fa0b0f6535cc2c2333dba88827a8d74d2
Mathieu <bon.mathieu@gmail.com>
2024-12-19 10:44:51 +0100

    Extended color theme instead of replacing it completely (stock tailwind css colors are available again)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		colors: {
-			green: {
-				light: "#FAFCE5",
-				// medium: "#D9E75F", // Même couleur que le logo original
-				medium: "#d0e322",
-				dark: "#5C6B2F"
-			},
-			gray: {
-				light: "#7D8189",
-				dark: "#333333"
-			},
-			white: "#ffffff",
-			extend: {}
+		extend: {
+			colors: {
+				green: {
+					light: "#FAFCE5",
+					// medium: "#D9E75F", // Même couleur que le logo original
+					medium: "#d0e322",
+					dark: "#5C6B2F"
+				},
+				gray: {
+					light: "#7D8189",
+					dark: "#333333"
+				},
+				white: "#ffffff"
+			}
 		},
 		plugins: []
 	}
