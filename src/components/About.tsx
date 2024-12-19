18bfe784a407fe5a0e849012c79f334cae28bbb2
Mathieu <bon.mathieu@gmail.com>
2024-12-19 17:16:44 +0100

    Inverted side of picture on md+ screens for About section.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -36,7 +36,7 @@ const About: React.FC = () => {
 			))} */}
 
 			<div className="about-paragraph grid py-2 md:py-8 mx-auto gap-2 md:gap-5 lg:gap-6 xl:gap-8 md:grid-cols-12">
-				<div className={`mr-auto md:col-span-7 flex flex-col justify-center `}>
+				<div className={`mr-auto md:col-span-7 md:order-2 flex flex-col justify-center `}>
 					<div>
 						<div className="about-paragraph-title text-xl font-bold mb-2">
 							{about.items[0].title}
@@ -54,12 +54,11 @@ const About: React.FC = () => {
 						</p>
 					</div>
 				</div>
-				<div className={`about-paragraph-media flex items-center mt-2 md:mt-0 md:col-span-5 md:flex`}>
+				<div className={`about-paragraph-media flex items-center mt-2 md:mt-0 md:col-span-5 md:order-1`}>
 					<img
 						src={about.items[0].img.src}
 						alt={about.items[0].img.alt}
 						className="object-cover max-h-48 md:max-h-max w-full"
-						// className="object-fit w-full h-full"
 					/>
 				</div>
 			</div>
